efb5f63ed5a2efddc8d787e6c578d7e94ea4e5e9
benitex-dev <benitezjorgedev@gmail.com>
2026-02-05 23:40:22 -0300

    agregamos el filtro al security config

--- src/main/java/com/example/hellow_world/security/config/SecurityConfig.java
+++ src/main/java/com/example/hellow_world/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.hellow_world.security.config;
 
+import com.example.hellow_world.security.config.filter.JwtTokenValidator;
+import com.example.hellow_world.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -19,6 +22,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,12 +31,19 @@ import java.util.List;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+
+
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf->csrf.disable())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class)
+
                 .build();
     }
 
